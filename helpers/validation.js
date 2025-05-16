abf12dfd5da702c46ee779c357fbe2df508e3b74
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-16 18:56:42 +0545

    check for image

--- helpers/validation.js
+++ helpers/validation.js
@@ -4,4 +4,14 @@ exports.signUpValidation = [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Email is required').isEmail().normalizeEmail({gmail_remove_dots: true }),
     check('password', 'Password is required').isLength(6),
+    check('image').custom((value, {req})=>{
+
+        if(req.file.mimetype == 'image/jpeg' || req.file.mimetype == 'image/png'){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }).withMessage('Please upload an image with type png, jpg'),
 ]
