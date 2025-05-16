a09b26fd5ce30f0f2e34be7ae8fee3b08856d77c
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-16 18:56:05 +0545

    add image upload functionality to user registration using multer

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {signUpValidation} = require('../helpers/validation');
 
-const userController = require('../controllers/userController');
-router.post('/register', signUpValidation, userController.register)
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../public/images"));
+  },
+  filename: function (req, file, cb) {
+    const name = Date.now() + "-" + file.originalname;
+    cb(null, name);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  file.mimetype === "image/jpeg" ||
+  file.mimetype === "image/png" ||
+  file.mimetype === "image/jpg"
+    ? cb(null, true)
+    : cb(new Error("Invalid file type"), false);
+};
+
+const upload = multer({ 
+    storage: storage,
+    fileFilter: fileFilter,
+});
+
+const { signUpValidation } = require("../helpers/validation");
+
+const userController = require("../controllers/userController");
+router.post(
+  "/register",
+  upload.single("image"),
+  signUpValidation,
+  userController.register
+);
 
 module.exports = router;
