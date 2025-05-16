8e78e6760a62f9a01a45d7d16d518b4a8f352fb3
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-16 18:24:31 +0545

    implement email verification by sending a token after registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,9 @@ const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const db = require('../config/dbConnection');
 
+const randomstring = require('randomstring')
+const sendMail = require('../helpers/sendMail')
+
 const register = (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -46,6 +49,20 @@ const register = (req, res) => {
                 error: err,
               });
             }
+
+            let mailSubject = 'Mail Verification';
+            const randomToken = randomstring.generate();
+            let content = '<p>Hello, '+req.body.name+', \ please <a href = "http://localhost:5000/mail-verification?token='+randomToken+'"> verify</a> your email address</p>'
+
+            sendMail(req.body.email, mailSubject, content);
+
+            db.query('UPDATE users set token = ? where email = ?', [randomToken, req.body.email, function(error, result, field){
+                if(error){
+                    return res.status(500).json({
+                        msg: err
+                    })
+                }
+            }])
 
             return res.status(201).json({
               message: "User registered successfully",
